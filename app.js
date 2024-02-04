dae03a4f635ee195aaaa869611c16418e45adb12
Md sifat <mdsifat7252@gmail.com>
2024-02-04 15:17:23 +0600

    build.33

--- app.js
+++ app.js
@@ -2,6 +2,37 @@ console.log("hello world");
 
 const generateForm = document.querySelector(".generate-form");
 const imgGalarry = document.querySelector(".img-galarry");
+const OPENAI_API_KEY = "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";
+
+const genaerteAiImg =  async(prmt , qnt) =>{
+    try {
+        // sending request to openai api to generate images based on user inputs 
+        const res  = await fetch("https://api.openai.com/v1/images/generations",{
+            method : "POST",
+            header : {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${OPENAI_API_KEY}`
+            },
+            body : JSON.stringify({
+                promt : prmt,
+                n : parseInt(qnt),
+                size : "512x512",
+                response_format : "b64_json"
+            })
+        });
+        if(!res.ok){
+            throw new Error("API credit ended! please buy more credits  ");
+        }
+
+        // get data from the response 
+        const {data} = await respose.json();
+        console.log(data);
+    }catch (error){
+        console.log(error.message);
+        alert(error.message);
+        
+    }
+}
 
 const handleFromSubmission = (e) =>{
     e.preventDefault();
